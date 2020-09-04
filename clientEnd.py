415fa3a60f53a7465fbac2f993ca46d73f981f5e
hispaniora <hispaniora@yahoo.com>
2020-09-04 17:20:16 +0800

    修改为可持续通信

--- clientEnd.py
+++ clientEnd.py
@@ -2,10 +2,10 @@ import socket
 
 # 客户端使用UDP时，无需建立连接，直接用sendto()给服务器发信息
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-print('客户端开始通信--')
+print('客户端——')
 while True:
-    data = input('请输入发送内容：')
+    data = input('自己：')
     s.sendto(data.encode(), ('127.0.0.1', 8099))  # 发送信息
     recvMsg = s.recv(1024)  # 接收返回的信息
-    print(recvMsg.decode())
+    print('服务器:', recvMsg.decode())
 s.close()
--- serverEnd.py
+++ serverEnd.py
@@ -4,12 +4,11 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # 绑定地址和端口
 s.bind(('127.0.0.1', 8099))
-print('客户端开始通信--')
+print('服务器端——')
 while True:
     # 接收数据，自动阻塞等待客户端请求
-    data,addr = s.recvfrom(1024)  # 接收客户端发送过来的数据内容和地址
+    data, addr = s.recvfrom(1024)  # 接收客户端发送过来的数据内容和地址
     data = data.decode()
-    print('客户端的IP地址：', addr)
-    print('客户端发过来的信息内容：%s'%data)
-    msg = input('给客户端的回复：')
+    print(addr, ':', data)
+    msg = input('回复：')
     s.sendto(msg.encode(), addr)  # 给客户端发送消息
