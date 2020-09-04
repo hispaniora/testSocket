0e89b38fd4fcc9ec53f1786b2ea686977d7e50a7
hispaniora <hispaniora@yahoo.com>
2020-09-04 10:34:30 +0800

    my first commit

---
+++ serverEnd.py
@@ -0,0 +1,15 @@
+import socket
+
+# SOCK_DGRAM指定了这个socket是UDP类型
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 绑定地址和端口
+s.bind(('127.0.0.1', 8099))
+print('开始通信--')
+while True:
+    # 接收数据，自动阻塞等待客户端请求
+    data,addr = s.recvfrom(1024)  # 接收客户端发送过来的数据内容和地址
+    data = data.decode()
+    print('客户端的IP地址：', addr)
+    print('客户端发过来的信息内容：%s'%data)
+    msg = input('给客户端的回复：')
+    s.sendto(msg.encode(), addr)  # 给客户端发送消息
