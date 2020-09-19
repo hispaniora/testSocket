61401b7071316ce63f0a08bf6ee8f0ac262adf28
hispaniora <hispaniora@yahoo.com>
2020-09-19 10:02:01 +0800

    完善了tcp

--- TCPClient.py
+++ TCPClient.py
@@ -1,12 +1,12 @@
 import socket
 
 s = socket.socket()
-s.connect(('192.168.1.103', 8099))  # 连接服务器端
+s.connect(('192.168.0.104', 8099))  # 连接服务器端
 while True:
     data = input('自己：')
     s.send(data.encode())  # 发送数据
     recvMsg = s.recv(1024).decode()
-    print(recvMsg)
+    print('服务器端：', recvMsg)
     if data == 'close':
         break
-    s.close()
+s.close()
--- TCPServer.py
+++ TCPServer.py
@@ -1,12 +1,11 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # SOCK_STREAM表示为TCP连接
-s.bind(('192.168.1.103', 8099))  # 服务器端绑定自己的IP和端口号
+s.bind(('192.168.0.104', 8099))  # 服务器端绑定自己的IP和端口号
 s.listen(128)  # 设置监听，同时能连接的客户端数
 while True:
     print('等待下一个客户端的消息……')
     client, addr = s.accept()  # 接收到客户端的socket和地址
-    print('接收到client数据', addr)
     while True:
         data = client.recv(1024)  # 获取客户端的信息
         data = data.decode()
